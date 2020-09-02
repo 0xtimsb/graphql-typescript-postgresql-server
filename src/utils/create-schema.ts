44dac64d649002bb78816e71fdacb989d94d076a
Smit Barmase <smitbarmase@outlook.com>
2020-09-02 18:07:57 +0530

    linux env fix.

--- src/utils/create-schema.ts
+++ src/utils/create-schema.ts
@@ -1,12 +1,12 @@
 import { buildSchema } from "type-graphql";
 
-import { ChangePasswordResolver } from "../resolvers/user/change-password";
-import { ForgotPasswordResolver } from "../resolvers/user/forgot-password";
-import { LoginResolver } from "../resolvers/user/login";
-import { LogoutResolver } from "../resolvers/user/logout";
-import { MeResolver } from "../resolvers/user/me";
-import { SignUpResolver } from "../resolvers/user/signup";
-import { ConfirmUserResolver } from "../resolvers/user/confirm-user";
+import { ChangePasswordResolver } from "../resolvers/user/change-password-resolver";
+import { ForgotPasswordResolver } from "../resolvers/user/forgot-password-resolver";
+import { LoginResolver } from "../resolvers/user/login-resolver";
+import { LogoutResolver } from "../resolvers/user/logout-resolver";
+import { MeResolver } from "../resolvers/user/me-resolver";
+import { SignUpResolver } from "../resolvers/user/signup-resolver";
+import { ConfirmUserResolver } from "../resolvers/user/confirm-user-resolver";
 
 export const createSchema = () => buildSchema({
   resolvers: [
